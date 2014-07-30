1693b6b4ed0e05d701c1a3a2f1675eb5c5392f6b
Peter Novotnak <pnovotnak@flexion.us>
2014-07-30 12:45:20 -0500

    fixed local settings template syntax

--- PlanetExpress/settings/local_settings-template.py
+++ PlanetExpress/settings/local_settings-template.py
@@ -4,10 +4,10 @@
 
 ACCOUNTS = {
     'rax-ord-ng': {
-        'AUTH_URL': 'https://identity.api.rackspacecloud.com/v2.0/'
-        'REGION': 'DFW'
-        'USERNAME': 'username'
-        'API_KEY': '00000000000000000000000'
+        'AUTH_URL': 'https://identity.api.rackspacecloud.com/v2.0/',
+        'REGION': 'DFW',
+        'USERNAME': 'username',
+        'API_KEY': '00000000000000000000000',
     }
 }
 
